4de4280ced55acfa60230578b7e86014acf31df0
Danis Canada <dcanadablandon@gmail.com>
2025-07-09 10:29:35 -0700

    add pagination
    add pagination logic to the posts view and created the pagination.html
    template.
    
    add also EmptyPage and NotAnInteger exception handle

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 
@@ -5,7 +6,24 @@ from .models import Post
 def post_list(request):
     """Retrieves the list of published posts."""
 
-    posts = Post.published.all()
+    posts_list = Post.published.all()
+    paginator = Paginator(posts_list,3)# pagination with 3 post per page
+    page_number = request.GET.get('page', 1)
+    
+    try:
+       
+        posts = paginator.page(page_number)
+
+    except EmptyPage:
+        #if the number is out of range show the las page
+        # paginator.num_pages = last page
+        posts = paginator.page(paginator.num_pages)
+    
+    # in the case page parameter is not an integer example  example.com/?page=asdf
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+
+
     return render(
         request, 
         'blog/post/list.html',
